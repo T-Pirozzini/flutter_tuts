777f42017bc16b68afe7bfa395b1f9f94497caf2
T-Pirozzini <tpirozzini@gmail.com>
2023-03-07 19:20:19 -0800

    setup for flutter quill

--- flutter_notes/lib/pages/editing_note_page.dart
+++ flutter_notes/lib/pages/editing_note_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_notes/models/note.dart';
+import 'package:flutter_quill/flutter_quill.dart';
 
 class EditingNotePage extends StatefulWidget {
   EditingNotePage({
@@ -15,6 +16,28 @@ class EditingNotePage extends StatefulWidget {
 }
 
 class _EditingNotePageState extends State<EditingNotePage> {
+  QuillController _controller = QuillController.basic();
+
+  @override
+  void initState() {
+    super.initState();
+    loadExistingNote();
+  }
+
+  // load existing note
+  void loadExistingNote() {
+    final doc = Document()..insert(0, widget.note.text);
+    setState(() {
+      _controller = QuillController(
+          document: doc, selection: const TextSelection.collapsed(offset: 0));
+    });
+  }
+
+  // add new note
+  void addNewNote(int i) {
+    
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold();
