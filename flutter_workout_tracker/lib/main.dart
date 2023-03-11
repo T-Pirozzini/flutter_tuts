ee5e463eca8740c95159b61994f546e21a149077
T-Pirozzini <tpirozzini@gmail.com>
2023-03-11 15:02:54 -0800

    setup hive and create date time object and functions for format conversions

--- flutter_workout_tracker/lib/main.dart
+++ flutter_workout_tracker/lib/main.dart
@@ -1,9 +1,15 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_workout_tracker/data/workout_data.dart';
 import 'package:flutter_workout_tracker/pages/home_page.dart';
+import 'package:hive_flutter/adapters.dart';
 import 'package:provider/provider.dart';
 
-void main() {
+void main() async {
+  // initialize hive
+  await Hive.initFlutter();
+  // open a hive box
+  await Hive.openBox("workout_database");
+
   runApp(const MyApp());
 }
 
