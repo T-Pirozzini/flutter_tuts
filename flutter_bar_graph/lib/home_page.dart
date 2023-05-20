6a66586d556eddee5b9f509d7986f5d729639681
T-Pirozzini <tpirozzini@gmail.com>
2023-05-19 20:23:13 -0700

    setup fl_chart project

---
+++ flutter_bar_graph/lib/home_page.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+import 'package:fl_chart/fl_chart.dart';
+
+/*
+TASK!
+
+Input: List of weekly expenses = [sunAMount, monAMount ]
+Output: Display in a nice bar graph
+
+*/
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  // list of weekly expenses
+  List<double> weeklySummary = [
+    4.40,
+    2.50,
+    42.42,
+    10.50,
+    100.20,
+    88.99,
+    90.10,
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold();
+  }
+}
