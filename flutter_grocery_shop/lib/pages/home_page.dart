62d4d06e3b499ad6ac55104fbdc52de00fc24f65
T-Pirozzini <tpirozzini@gmail.com>
2023-05-23 20:26:01 -0700

    setup grocery item tiles and the cart model

--- flutter_grocery_shop/lib/pages/home_page.dart
+++ flutter_grocery_shop/lib/pages/home_page.dart
@@ -1,10 +1,70 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+import '../components/grocery_item_tile.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      body: SafeArea(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            const SizedBox(height: 24),
+            // good morning
+            const Padding(
+              padding: EdgeInsets.symmetric(horizontal: 24.0),
+              child: Text("Good Morning,"),
+            ),
+
+            const SizedBox(height: 4),
+
+            // lets order fresh items for you
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 24.0),
+              child: Text(
+                "Let's order fresh items for you",
+                style: GoogleFonts.notoSerif(
+                  fontSize: 36,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ),
+
+            // divider
+            const Padding(
+              padding: EdgeInsets.symmetric(horizontal: 8.0),
+              child: Divider(),
+            ),
+
+            const SizedBox(height: 24),
+
+            // fresh items + grid
+            const Padding(
+              padding: EdgeInsets.symmetric(horizontal: 24.0),
+              child: Text(
+                'Fresh Items',
+                style: TextStyle(fontSize: 16),
+              ),
+            ),
+
+            // Expanded(
+            //   child: GridView.builder(
+            //     gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+            //         crossAxisCount: 2),
+            //     itemBuilder: (context, index) {
+            //       return GroceryItemTile(
+                    
+            //       );
+            //     },
+            //   ),
+            // )
+          ],
+        ),
+      ),
+    );
   }
 }
