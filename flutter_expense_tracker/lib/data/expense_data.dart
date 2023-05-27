4c00986143e70db037060d730b91d1954826f878
T-Pirozzini <tpirozzini@gmail.com>
2023-05-27 12:35:24 -0700

    initial setup for expense tracker app

---
+++ flutter_expense_tracker/lib/data/expense_data.dart
@@ -0,0 +1,30 @@
+class ExpenseData {
+  // list of all expenses
+
+  // get expense list
+
+  // add new expense
+
+  // delete expense
+
+  // get weekday (mon, tues, wed, etc.) from a dateTimeobject
+
+  // get date for the start of the week
+
+  /*
+  convert overall list of expenses into a daily expense summary
+  e.g.
+
+  overallExpenseList = 
+  [
+    [food, 2023/01/30], $10],
+    [hat, 2023/01/30], $15],
+    [drinks, 2023/01/31], $1],
+    [food, 2023/01/30], $10],
+    [food, 2023/01/30], $10],
+    [food, 2023/01/30], $10],
+    [food, 2023/01/30], $10],
+
+  ]
+  */
+}
---
+++ flutter_expense_tracker/lib/models/expense_item.dart
@@ -0,0 +1,11 @@
+class ExpenseItem {
+  final String name;
+  final String amount;  
+  final DateTime date;
+
+  const ExpenseItem({    
+    required this.name,
+    required this.amount,
+    required this.date,
+  });
+}
