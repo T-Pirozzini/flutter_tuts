ae4b314d9da9ecc00c566b433acde721412332ba
T-Pirozzini <tpirozzini@gmail.com>
2024-05-06 20:14:36 -0700

    generate change notifiers and dependent providers

--- flutter-riverpod-tutorial-starter-project/lib/screens/cart/cart_screen.dart
+++ flutter-riverpod-tutorial-starter-project/lib/screens/cart/cart_screen.dart
@@ -16,6 +16,7 @@ class _CartScreenState extends ConsumerState<CartScreen> {
   @override
   Widget build(BuildContext context) {
     final cartProducts = ref.watch(cartNotifierProvider);
+    final total = ref.watch(cartTotalProvider);
 
     return Scaffold(
       appBar: AppBar(
@@ -43,6 +44,7 @@ class _CartScreenState extends ConsumerState<CartScreen> {
               }).toList(),
               // output cart products here
             ),
+            Text('Total price - \$${total.toStringAsFixed(2)}'),
 
             // output totals here
           ],
