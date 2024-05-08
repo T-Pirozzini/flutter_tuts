5a9aec13f9b3ba1d37e0bad6195544f9de4a11ea
T-Pirozzini <tpirozzini@gmail.com>
2024-05-08 16:40:46 -0700

    setup rpg project and setup the theme for colors, texts, and google fonts import

---
+++ flutter_rpg_tut/lib/main.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_rpg_tut/screens/home/home.dart';
+import 'package:flutter_rpg_tut/theme.dart';
+
+void main() {
+  runApp(MaterialApp(
+    theme: primaryTheme,
+    home: const Home(),
+  ));
+}
+
+class Sandbox extends StatelessWidget {
+  const Sandbox({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: const Text('Sandbox'),
+          centerTitle: true,
+          backgroundColor: Colors.grey,
+        ),
+        body: const Center(child: Text('sandbox')));
+  }
+}
